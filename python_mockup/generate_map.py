2cb1164b12d2f2bf34983453ab204a335604be98
Eric <ericl16384@gmail.com>
2023-09-04 09:36:34 -0700

    map generation working

--- python_mockup/generate_map.py
+++ python_mockup/generate_map.py
@@ -1,40 +1,45 @@
 import random
 
-NO_ORE = "no ore"
-IRON_ORE = "iron ore"
-COPPER_ORE = "copper ore"
-
 class Map:
-    def __init__(self) -> None:
-        self.lookup = {}
-        self.seed = random.randint(0, 2**64-1)
     
-    def get_natural_tile(self, x, y):
+    NO_ORE = "no ore"
+    IRON_ORE = "iron ore"
+    COPPER_ORE = "copper ore"
+
+    def __init__(self) -> None:
+        self.seed = random.randint(0, 2**64-1)
+        self.tiles = {}
+    
+    def get_generated_tile(self, x, y):
+        assert isinstance(x, int)
+        assert isinstance(y, int)
+
         pos = (x, y)
 
         random.seed(f"{self.seed}{pos}")
 
         r = random.random()
 
-        if r < 0.1:
-            t = IRON_ORE
-        elif r < 0.2:
-            t = COPPER_ORE
+        if r < 0.05:
+            t = self.IRON_ORE
+        elif r < 0.10:
+            t = self.COPPER_ORE
         else:
-            t = NO_ORE
+            t = self.NO_ORE
         
         return t
     
-    # def generate(self, min_x, min_y, max_x, max_y):
+    # def generate_tile(self, x, y):
+    #     pos = (x, y)
+    #     self.tiles[pos] = self.get_generated_tile(*pos)
 
+    # def generate_area(self, min_x, min_y, max_x, max_y):
+    #     pass
 
     def get_tile(self, x, y):
-        assert isinstance(x, int)
-        assert isinstance(y, int)
-
         pos = (x, y)
 
-        if pos not in self.lookup:
-            self.lookup[pos] = self.get_natural_tile(*pos)
+        if pos not in self.tiles:
+            self.tiles[pos] = self.get_generated_tile(*pos)
 
-        return self.lookup[pos]
+        return self.tiles[pos]
--- python_mockup/main.py
+++ python_mockup/main.py
@@ -1,47 +1,66 @@
+import generate_map, base_controller
+
+def reload():
+    global map
+    map = generate_map.Map()
+reload()
+
+# print(map.lookup)
+# map.get_tile(0, 0)
+
+# print(map.lookup)
+# map.get_tile(10, 10)
+
+# print(map.lookup)
+
+MAP_SIZE = (15, 15)
+TILE_DISPLAY_SIZE = 40
+
+
+
+
+
+
 import pygame
 
+pygame.init()
 
-
-
-
-import generate_map
-
-game_map = generate_map.Map()
-
-print(game_map.lookup)
-game_map.get_tile(0, 0)
-
-print(game_map.lookup)
-game_map.get_tile(10, 10)
-
-print(game_map.lookup)
-
-
-
-
-
-input()
-
-
-
-
-screen = pygame.display.set_mode((1500, 750))
-
+SCREEN = pygame.display.set_mode((1500, 750))
 pygame.display.set_caption("main.py")
+CLOCK = pygame.time.Clock()
 
-clock = pygame.time.Clock()
 
-while not done:
-    clock.tick(60)
+while pygame.get_init():
+    CLOCK.tick(60)
+
+
+    SCREEN.fill("black")
+
+    for x in range(MAP_SIZE[0]):
+        for y in range(MAP_SIZE[1]):
+            t = map.get_tile(x, y)
+
+            if t == map.NO_ORE:
+                c = "green"
+            elif t == map.IRON_ORE:
+                c = "blue"
+            elif t == map.COPPER_ORE:
+                c = "orange"
+            else:
+                c = "pink"
+
+            dx = x * TILE_DISPLAY_SIZE + 50
+            dy = y * TILE_DISPLAY_SIZE + 50
+            
+            pygame.draw.rect(SCREEN, c, ((dx, dy), (TILE_DISPLAY_SIZE,TILE_DISPLAY_SIZE)))
+
+    pygame.display.flip()
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-
-
-    screen.fill("black")
-
-    pygame.display.flip()
-
-
-pygame.quit()
+            pygame.quit()
+        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                reload()
