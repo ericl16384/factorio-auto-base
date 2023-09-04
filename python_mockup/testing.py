1b369ff641557249d79afb128396a5f950f7157c
Eric <ericl16384@gmail.com>
2023-09-04 11:47:22 -0700

    redesigned algorithm for use with combinators!

--- python_mockup/testing.py
+++ python_mockup/testing.py
@@ -1,60 +1,74 @@
 all_previous = set()
 
-MOVE_LOOKUP = ((0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1))
+# MOVE_LOOKUP = ((0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1))
 
 x = 0
 y = 0
-
 layer = 0
-next_move = 0
+
+next_move_x = 0
+next_move_y = 0
+new_layer = 0
 
 for n in range(26):
-    next_move_x, next_move_y = MOVE_LOOKUP[next_move]
-
-    print(f"move={next_move}\t({next_move_x},\t{next_move_y})")
+    print(f"move=\t({next_move_x},\t{next_move_y})")
 
     x += next_move_x
     y += next_move_y
 
     print(f"node={n}\t({x},\t{y})\t{layer}")
 
+
     assert (x, y) not in all_previous
     all_previous.add((x, y))
+
 
     input()
     # print()
 
 
-    # start
-    if next_move == 0:
-        next_move = 1
-        layer += 1
-        print(f"move switched: {next_move}")
-    
     # new layer
-    elif x == layer and y == -1:
-        next_move = 5
-        layer += 1
-        print(f"move switched: {next_move}")
-    
+    if new_layer:
+        next_move_y -= next_move_y
+        new_layer -= new_layer
+
     # right side
-    elif x == layer and next_move == 1 or next_move == 5:
-        next_move = 2
-        print(f"move switched: {next_move}")
+    if x == layer and next_move_x > 0:
+        next_move_x -= 1
+        next_move_y += 1
+        print(f"move switched")
     
     # top side
-    elif y == layer and next_move == 2:
-        next_move = 3
-        print(f"move switched: {next_move}")
+    if y == layer and next_move_y > 0:
+        next_move_x -= 1
+        next_move_y -= 1
+        print(f"move switched")
     
     # left side
-    elif x == -layer and next_move == 3:
-        next_move = 4
-        print(f"move switched: {next_move}")
+    if x == -layer and next_move_x < 0:
+        next_move_x += 1
+        next_move_y -= 1
+        print(f"move switched")
     
     # bottom side
-    elif y == -layer and next_move == 4:
-        next_move = 1
-        print(f"move switched: {next_move}")
+    if y == -layer and next_move_y < 0:
+        next_move_x += 1
+        next_move_y += 1
+        print(f"move switched")
+
+    # start
+    if next_move_x == 0 and next_move_y == 0:
+        new_layer += 1
+    
+    # new layer
+    if x == layer and y == -1:
+        new_layer += 1
+    
+    if new_layer:
+        next_move_x -= next_move_x
+        next_move_y -= next_move_y
+        layer += 1
+        next_move_x = layer - x
+        next_move_y = -y
 
     
